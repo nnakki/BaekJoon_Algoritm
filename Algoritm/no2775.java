c09c0fe2df67aff7055969316a58321ec7ca2ff8
nnakki <yvely225@gmail.com>
2022-05-17 12:27:15 +0900

    백준 2775 부녀회장이 될테야

--- Algoritm/no2775.java
+++ Algoritm/no2775.java
@@ -2,12 +2,14 @@ package Algoritm;
 
 import java.util.Scanner;
 
+//부녀회장이 될테야
 public class no2775 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         int result = 0;
 
+        //todo 아직못품
         while (T-- > 0) {
             int k = sc.nextInt();
             for (int i = 0; i < k; i++) {

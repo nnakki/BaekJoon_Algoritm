758e58cb1497144a3ce174b263ead7a49032536c
nnakki <yvely225@gmail.com>
2022-05-17 20:43:12 +0900

    백준 10845 큐 (완)

---
+++ Algoritm/no10845.java
@@ -0,0 +1,70 @@
+package Algoritm;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+//큐
+//push X: 정수 X를 큐에 넣는 연산이다.
+//pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+//size: 큐에 들어있는 정수의 개수를 출력한다.
+//empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
+//front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+//back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+public class no10845 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Queue<Integer> queue = new LinkedList<>();
+
+        int N = Integer.parseInt(br.readLine());
+        int rear = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+
+            switch (str) {
+                case "push":
+                    String str2 = st.nextToken();
+                    queue.offer(Integer.parseInt(str2));
+                    rear = Integer.parseInt(str2);
+                    break;
+
+                case "pop":
+                    if (queue.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(queue.poll() + "\n");
+                    }
+                    break;
+
+                case "size":
+                    bw.write(queue.size() + "\n");
+                    break;
+
+                case "empty":
+                    bw.write(queue.isEmpty() ? "1\n" : "0\n");
+                    break;
+
+                case "front":
+                    if (queue.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(queue.peek() + "\n");
+                    }
+                    break;
+                case "back":
+                    if (queue.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(rear + "\n");
+                    }
+                    break;
+            }
+        }
+        bw.flush();
+    }
+}

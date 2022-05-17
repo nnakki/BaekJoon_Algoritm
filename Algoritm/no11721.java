4d3c9d2c9893105237d9cad629c156d2949809f9
nnakki <yvely225@gmail.com>
2022-05-17 12:26:30 +0900

    백준 11721 열글자씩 끊어 출력

--- Algoritm/no11721.java
+++ Algoritm/no11721.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import static java.lang.System.in;
 
+// 열 글자씩 끊어찍기
 public class no11721 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(in);

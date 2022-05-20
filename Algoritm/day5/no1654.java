b7d4c378a1122f88bb796e9e1f81d21287153492
nnakki <yvely225@gmail.com>
2022-05-20 22:36:52 +0900

    백준 1654 랜선자르기 (완_이진탐색 / 여러번보세요)

--- Algoritm/day5/no1654.java
+++ Algoritm/day5/no1654.java
@@ -1,8 +1,7 @@
 package Algoritm.day5;
 
+
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class no1654 {
@@ -11,31 +10,44 @@ public class no1654 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        //현재 가진 랜선의 갯수
         int K = Integer.parseInt(st.nextToken());
+        //만들어야 할 랜선의 갯수
         int N = Integer.parseInt(st.nextToken());
-        ArrayList<Integer> arr = new ArrayList<>();
 
-        for (int i = 0; i < K; i++) {
-            int num = Integer.parseInt(br.readLine());
-            arr.add(i, num);
-        }
+        //들어오는 랜선 값들을 저장할 배열
+        int[] array = new int[K];
+        //랜선 중 가장 긴 길이의 랜선
+        long maxLength = 0;
 
-        Collections.sort(arr);
-
-        int targetLength = arr.get(0);
-        int countSum = 0;
-
-        while (countSum < N) {
-            for (int i = 0; i < K; i++) {
-                int countByLine = arr.get(i) / targetLength;
-                countSum += countByLine;
-            }
-            if (countSum < N) {
-                targetLength -= 1;
-                countSum = 0;
+        for (int i = 0; i < array.length; i++) {
+            int number = Integer.parseInt(br.readLine());
+            array[i] = number;
+            if (maxLength < array[i]) {
+                maxLength = array[i];
             }
         }
-        System.out.println(targetLength);
+
+        maxLength++;
+        //이진탐색
+        long minLength = 0;
+        long middle = 0;
+
+        while (minLength < maxLength) {
+            middle = (maxLength + minLength) / 2;
+            long cnt = 0;
+
+            for (int i = 0; i < array.length; i++) {
+                cnt += (array[i] / middle);
+            }
+
+            if (cnt < N) {
+                maxLength = middle;
+            } else {
+                minLength = middle + 1;
+            }
+        }
+        System.out.println(minLength - 1);
     }
 }
 

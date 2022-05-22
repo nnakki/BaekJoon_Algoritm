90be1f5a48381d2bc5ea1a9758d085047f731222
nnakki <yvely225@gmail.com>
2022-05-22 13:08:47 +0900

    백준 8979 올림픽 (완)

--- Algoritm/day6/no8979.java
+++ Algoritm/day6/no8979.java
@@ -6,24 +6,44 @@ import java.util.StringTokenizer;
 public class no8979 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());    // 총 참여 국가 수
         int K = Integer.parseInt(st.nextToken());    // 알고 싶은 국가 넘버
 
-        int[][] olympicReuslt = new int[N][4];      // 국가의 메달 결과를 담은 NX4 행렬 만들기
+        int[][] result = new int[N - 1][3];      // 국가의 메달 결과를 담은 NX4 행렬 만들기
+        int temp = 0;
+        int[] target = new int[3];
 
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
-            st = new StringTokenizer(str);
-            for (int j = 0; j < 4; j++) {
-                int result = Integer.parseInt(st.nextToken());
-                olympicReuslt[i][j] = result;
+            st = new StringTokenizer(str); // 1 1 2 0
+
+            if (Integer.parseInt(st.nextToken()) == K) {
+                for (int j = 0; j < 3; j++) {
+                    target[j] = Integer.parseInt(st.nextToken());
+                }
+            } else {
+                for (int j = 0; j < 3; j++) {
+                    int medal_result = Integer.parseInt(st.nextToken());
+                    result[temp][j] = medal_result;
+                }
+                temp++;
             }
         }
 
+        int ranking = 1;    // 비교하고자 하는 나라의 순위를 저장할 변수
         // 금메달 개수 비교
-    }
 
+        for (int i = 0; i < N - 1; i++) {
+            if (result[i][0] > target[0]) {
+                ranking++;
+            } else if (result[i][0] == target[0] && result[i][1] > target[1]) {
+                ranking++;
+            } else if (result[i][0] == target[0] && result[i][1] == target[1] && result[i][2] > target[2]) {
+                ranking++;
+            }
+        }
+        System.out.println(ranking);
+    }
 }
